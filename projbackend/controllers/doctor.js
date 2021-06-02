d167876f58bc91d6dfd8a12aa28d2ab4b20203f4
Jaya Sowmini <jayasowminikona4901@gmail.com>
2021-06-02 20:02:45 +0530

    doctor CRUD

--- projbackend/controllers/doctor.js
+++ projbackend/controllers/doctor.js
@@ -40,7 +40,7 @@ exports.createDoctor=(req, res)=>{
   
   //Todo restrictions on field
   let doctor=new Doctor(fields);
-  
+
 
   //handle file in here
   if(file.photo){
@@ -66,3 +66,81 @@ exports.createDoctor=(req, res)=>{
 
      });
 };
+
+exports.getDoctor=(req, res)=>{
+    req.doctor.photo=undefined;
+    return res.json(req.doctor);
+}
+
+
+//middlewares
+exports.photo = (req, res, next)=>{
+    if(req.doctor.photo.data){
+        res.set("Content-Type", req.doctor.photo.contentType)
+        return res.send(req.doctor.photo.data);
+    }
+    next();
+}
+
+//deleting the doctor
+
+exports.deleteDoctor = (req, res)=>{
+    let doctor = req.doctor;
+    doctor.remove((err, deletedDoctor)=>{
+        if(err){
+            return res.status(400).json({
+                error: "Failed to delete the dcotor"
+            })
+        }
+  
+        res.json({
+            message: "Deletion was a success",
+            deletedDoctor
+        })
+    })
+}
+
+
+//updating the doctor
+exports.updateDoctor = (req, res)=>{
+    let form = new formidable.IncomingForm();
+    form.keepExtensions=true;
+
+    form.parse(req, (err, fields, file)=>{
+     if(err){
+         return res.status(400).json({
+             error: "Problem with the image"
+         })
+     }
+
+ 
+ //Todo restrictions on field
+ let doctor=req.doctor;
+ doctor = _.extend(doctor, fields);
+
+
+ //handle file in here
+ if(file.photo){
+     if(file.photo.size > 3000000){
+         return res.status(400).json({
+             error:"file size is too big!"
+         })
+     }
+     doctor.photo.data = fs.readFileSync(file.photo.path);
+     doctor.photo.contentType = file.photo.type;
+ }
+
+ //save to the DB
+ doctor.save((err, doctor)=>{
+     if(err){
+         return res.status(400).json({
+             error:"updating of  doctor in DB failed"
+         })
+     }
+
+     res.json(doctor);
+ });
+
+    });
+}
+
--- projbackend/routes/doctor.js
+++ projbackend/routes/doctor.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router=express.Router();
 
-const {getDoctorById, createDoctor}=require("../controllers/doctor");
+const {getDoctorById, createDoctor, getDoctor, photo, deleteDoctor, updateDoctor}=require("../controllers/doctor");
 const {isSignedIn, isAuthenticated, isAdmin}=require("../controllers/auth");
 const {getCategoryById}=require("../controllers/category");
 const {getUserById}=require("../controllers/user");
@@ -10,8 +10,20 @@ router.param("userId",getUserById);
 //router.param("categoryId",getCategoryById);
 router.param("doctorId",getDoctorById);
 
-//routes
+//create routes
 router.post("/doctor/create/:userId",isSignedIn, isAuthenticated, isAdmin, createDoctor);
+//read routes
+router.get("/doctor/:doctorId", getDoctor);
+router.get("/doctor/photo/:doctorId", photo);
+
+//delete route
+router.delete("/doctor/:doctorId/:userId",isSignedIn,isAuthenticated,isAdmin, deleteDoctor);
+
+//update route
+router.put("/doctor/:doctorId/:userId",isSignedIn,isAuthenticated,isAdmin,updateDoctor);
+
+//listing route
+
 
 
 module.exports=router;
