ba1ad65ddc779e380943b129b03344748c118bc6
Jaya Sowmini <jayasowminikona4901@gmail.com>
2021-06-02 19:40:27 +0530

    storing doctor

--- projbackend/controllers/doctor.js
+++ projbackend/controllers/doctor.js
@@ -1,4 +1,8 @@
 const Doctor = require("../models/doctor");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
+
 
 exports.getDoctorById=(req, res, next, id)=>{
     Doctor.findById(id)
@@ -15,5 +19,50 @@ exports.getDoctorById=(req, res, next, id)=>{
 
 
 exports.createDoctor=(req, res)=>{
-    
-}
+     let form = new formidable.IncomingForm();
+     form.keepExtensions=true;
+
+     form.parse(req, (err, fields, file)=>{
+      if(err){
+          return res.status(400).json({
+              error: "Problem with the image"
+          })
+      }
+
+      //destructure the fields
+  const {name, description, fees, category, available}=fields;
+  
+  if(!name || !description || !fees || !category || !available){
+         return res.status(400).json({
+             error:"Some fields are missing"
+         });
+  }
+  
+  //Todo restrictions on field
+  let doctor=new Doctor(fields);
+  
+
+  //handle file in here
+  if(file.photo){
+      if(file.photo.size > 3000000){
+          return res.status(400).json({
+              error:"file size is too big!"
+          })
+      }
+      doctor.photo.data = fs.readFileSync(file.photo.path);
+      doctor.photo.contentType = file.photo.type;
+  }
+
+  //save to the DB
+  doctor.save((err, doctor)=>{
+      if(err){
+          return res.status(400).json({
+              error:"Saving doctor in DB failed"
+          })
+      }
+
+      res.json(doctor);
+  })
+
+     });
+};
